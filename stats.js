da0f6af4ab9db2aa40edfc0c803c5809f3007b2f
sarahololuo <sarah.ololuo@ada.ac.uk>
2024-10-01 10:52:46 +0000

    deleted drafts

--- stats.js
+++ stats.js
@@ -10,25 +10,15 @@ function deleteExistingOutputFile() {
   }
 }
 
-// function processData() {
-//   const data = fs.readFileSync(inputFile, "utf-8");
-//   const lines = data.split(/\n/);
-
-//   for (line of lines) {
-//     elements = line.split(delimiter);
-//     console.log(elements);
-//   }
-// }
 
 
 function processData() {
   const data = fs.readFileSync(inputFile, "utf-8");
   // console.log(data)
   const lines = data.split(/\n/);
-  let passwordLengthArray = [];
 
-  const newObject = {
-  }
+
+  const newObject = {}
 
   for (const line of lines) { // loop through each line in lines
     let elements = line.split(delimiter); //split line into an array
@@ -41,18 +31,6 @@ function processData() {
       newObject[`${passwordLength} chars long`] = 1
     }
   }
-
-  
-
-  // for (const number in passwordLengthArray) {
-  //   console.log(newObjectPropertiesArray)
-  //   if (newObjectPropertiesArray.includes(`${number} chars long`)) {
-  //     newObject[`${number} chars long`] += 1;
-  //   } else {
-  //     newObject[`${number} chars long`] = 0;
-  //     newObject[`${number} chars long`] += 1;
-  //   }
-  // }
 
   console.log(newObject)
 }
