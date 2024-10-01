84d6036dbba42733c56a6ed1c85cd95f96ade610
sarahololuo <sarah.ololuo@ada.ac.uk>
2024-10-01 10:54:25 +0000

    removed comments

--- stats.js
+++ stats.js
@@ -14,17 +14,15 @@ function deleteExistingOutputFile() {
 
 function processData() {
   const data = fs.readFileSync(inputFile, "utf-8");
-  // console.log(data)
   const lines = data.split(/\n/);
 
 
   const newObject = {}
 
-  for (const line of lines) { // loop through each line in lines
-    let elements = line.split(delimiter); //split line into an array
-    const passwordLength = elements[1].length // access the password in line and return its length
-    passwordLengthArray.push(passwordLength) // push the length to password length array
-
+  for (const line of lines) { 
+    let elements = line.split(delimiter); 
+    const passwordLength = elements[1].length 
+ 
     if (newObject[`${passwordLength} chars long`]) {
       newObject[`${passwordLength} chars long`] += 1
     } else {
