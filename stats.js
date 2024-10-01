22721c4dc3ea415a7e8fff155881fe29a5deb16b
sarahololuo <sarah.ololuo@ada.ac.uk>
2024-10-01 10:52:13 +0000

    first attempt drafts

--- stats.js
+++ stats.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const inputFile = "10000-common-passwords.csv";
+const inputFile = "10000-most-common-passwords.csv";
 const outputFile = "statistics.csv";
 const delimiter = ",";
 
@@ -10,16 +10,56 @@ function deleteExistingOutputFile() {
   }
 }
 
+// function processData() {
+//   const data = fs.readFileSync(inputFile, "utf-8");
+//   const lines = data.split(/\n/);
+
+//   for (line of lines) {
+//     elements = line.split(delimiter);
+//     console.log(elements);
+//   }
+// }
+
+
 function processData() {
   const data = fs.readFileSync(inputFile, "utf-8");
+  // console.log(data)
   const lines = data.split(/\n/);
+  let passwordLengthArray = [];
 
-  for (let line of lines) {
-    elements = line.split(delimiter);
-    console.log(elements);
+  const newObject = {
   }
+
+  for (const line of lines) { // loop through each line in lines
+    let elements = line.split(delimiter); //split line into an array
+    const passwordLength = elements[1].length // access the password in line and return its length
+    passwordLengthArray.push(passwordLength) // push the length to password length array
+
+    if (newObject[`${passwordLength} chars long`]) {
+      newObject[`${passwordLength} chars long`] += 1
+    } else {
+      newObject[`${passwordLength} chars long`] = 1
+    }
+  }
+
+  
+
+  // for (const number in passwordLengthArray) {
+  //   console.log(newObjectPropertiesArray)
+  //   if (newObjectPropertiesArray.includes(`${number} chars long`)) {
+  //     newObject[`${number} chars long`] += 1;
+  //   } else {
+  //     newObject[`${number} chars long`] = 0;
+  //     newObject[`${number} chars long`] += 1;
+  //   }
+  // }
+
+  console.log(newObject)
 }
 
+
+
+
 // Main execution
-deleteExistingOutputFile(); 
+// deleteExistingOutputFile(); 
 processData();
